ff7d12064dda7c5bea4a0dd51b3cd0e4344ae59f
Juan Eulufi <juan.eulifi.sa@gmail.com>
2023-03-19 20:49:40 -0300

    flex hero

--- src/pages/Aplicacion.jsx
+++ src/pages/Aplicacion.jsx
@@ -1,7 +1,80 @@
-import React from 'react'
+import React from "react";
 
 export const Aplicacion = () => {
   return (
-    <div>Aplicacion</div>
-  )
-}
+    <section>
+      <div class="px-4 py-12 mx-auto max-w-7xl sm:px-6 md:px-12 lg:px-24 lg:py-24">
+        <div class="flex flex-wrap items-center mx-auto max-w-7xl flex-col-reverse md:flex-row-reverse">
+          <div class="w-full lg:max-w-lg lg:w-1/2 rounded-xl">
+            <div>
+              <div class="relative w-full max-w-lg">
+                <div class="absolute top-0 rounded-full bg-violet-300 -left-4 w-72 h-72 mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+
+                <div class="absolute rounded-full bg-fuchsia-300 -bottom-24 right-20 w-72 h-72 mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+                <div class="relative">
+                  <img
+                    class="object-cover object-center mx-auto rounded-lg shadow-2xl"
+                    alt="hero"
+                    src="https://picsum.photos/250"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col items-start mt-12 mb-16 text-left lg:flex-grow lg:w-1/2 lg:pl-6 xl:pl-24 md:mb-0 xl:mt-0">
+            <span class="mb-8 text-xs font-bold tracking-widest text-[#08D9D6] uppercase">
+              {" "}
+              Picturai{" "}
+            </span>
+            <h1 class="mb-8 text-4xl font-bold leading-none tracking-tighter text-[#FF2E63] md:text-7xl lg:text-5xl">
+              Crea imagenes a travez de IA
+            </h1>
+            <p class="mb-8 text-base leading-relaxed text-left text-[#EAEAEA]">
+              Create an image from text prompt
+            </p>
+            <form className="w-full">
+              <label
+                for="default-search"
+                class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+              >
+                Search
+              </label>
+              <div class="relative">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <svg
+                    aria-hidden="true"
+                    class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    ></path>
+                  </svg>
+                </div>
+                <input
+                  type="search"
+                  id="default-search"
+                  class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search Mockups, Logos..."
+                  required
+                />
+                <button
+                  type="submit"
+                  class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Search
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
