fdeb318d80f4c0e35ca78bf51f9057849c0ee829
Juan Eulufi <juan.eulifi.sa@gmail.com>
2023-03-19 19:20:02 -0300

    Layout para react-router-dom

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,8 +3,8 @@ import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
   return (
-    <div class="p-4 py-20 sm:ml-64">
-      <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+    <div class="p-4 pt-20 sm:ml-64">
+      <div class="p-4 rounded-lg dark:border-gray-700">
         <div class="gap-4 mb-4"><Outlet /></div>
       </div>
     </div>
