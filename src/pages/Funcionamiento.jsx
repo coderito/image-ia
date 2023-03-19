ecb1cfc2fd53d765ea2267b5a788bea3531bfe5e
Juan Eulufi <juan.eulifi.sa@gmail.com>
2023-03-19 19:20:46 -0300

    Agregando parrafos

--- src/pages/Funcionamiento.jsx
+++ src/pages/Funcionamiento.jsx
@@ -2,6 +2,36 @@ import React from 'react'
 
 export const Funcionamiento = () => {
   return (
-    <div>Funcionamiento</div>
+    <div>
+    <h1 class="mb-4 text-xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Buscamoser ser la aplicacion <span class="text-blue-600 dark:text-blue-500">#1</span> en experiencia de usuario.</h1>
+
+      <div className="text-justify">
+        <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+          Este fascinante proyecto representa una amalgama perfecta de
+          vanguardia tecnológica en el ámbito del aprendizaje automático y la
+          creatividad visual del diseño gráfico, lo que permite la creación de
+          imágenes sorprendentes, verosímiles y de alta calidad de manera
+          totalmente automatizada, gracias al uso de la extraordinaria
+          tecnología de OpenAI ❤️.
+        </p>
+        <br />
+        <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+          Mediante el uso de algoritmos de aprendizaje automático avanzados y
+          técnicas innovadoras de diseño gráfico, este proyecto logra combinar
+          la precisión y el realismo de la inteligencia artificial con la
+          creatividad y la habilidad artística humana, para producir imágenes
+          impresionantes que satisfacen las necesidades de cualquier usuario
+          exigente.
+        </p>
+      </div>
+      <div className="pt-8">
+        <button
+          type="button"
+          class="text-white font-bold bg-gradient-to-br from-sky-400 to-emerald-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+        >
+          Ir a la Aplicacion    
+        </button>
+      </div>
+    </div>
   )
 }
